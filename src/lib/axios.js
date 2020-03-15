6df03ba0b80b8bc4579027a942f27d64baa0a359
lc <innovgame@163.com>
2020-03-15 22:47:46 +0800

    设置axios请求库

---
+++ src/lib/axios.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import {
+  message
+} from 'antd';
+
+const instance = axios.create({
+  timeout: 2000
+});
+
+//拦截请求
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.common['Authorization'] = "Token" + token;
+    }
+    return config;
+  },
+  error => {
+    message.error('bad request');
+    return Promise.reject(error);
+  }
+);
+
+//拦截响应
+instance.interceptors.response.use(
+  response => {
+    return response.data;
+  },
+  error => {
+    message.error(error.response.statusText);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import axios from "@/lib/axios";
 
 const Home = () => {
+  axios.request("/hello");
   return <div>welcome home page</div>;
 };
 
