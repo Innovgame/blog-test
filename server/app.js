ead2c969247444caef89d5664852c5ccc75afe67
lc <innovgame@163.com>
2020-03-21 11:53:58 +0800

    Add 添加 mysql 和 sequelize, 并划分文件夹结构

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require("koa2-cors");
 const logger = require("koa-logger");
 
 const router = require('./router');
+const db = require('./models');
 
 const app = new Koa();
 
@@ -14,5 +15,16 @@ app
 
 app.use(router.routes(), router.allowedMethods());
 app.listen(5000, () => {
+  db.sequelize
+    .sync({
+      force: true
+    })
+    .then(() => {
+      console.log('sequelize connect success')
+      console.log('sever listen on http://127.0.0.1:5000')
+    })
+    .catch(err => {
+      console.error(err)
+    })
   console.info('sever listen on http://127.0.0.1:5000');
 });
